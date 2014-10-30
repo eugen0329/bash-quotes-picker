2f8192827fc0f882414aa226565ae79c828f417a
eugen0329 <eugen0329@gmail.com>
2014-10-30 03:28:29 +0300

    all actions moved to the block

--- bqpicker.rb
+++ bqpicker.rb
@@ -4,10 +4,10 @@ require "pry"
 require "colorize"
 require "mechanize"
 
-page = Mechanize.new.get("http://bashorg.org/")
-
-page.parser.css('br').each { |br| br.replace("\n") }
-page.parser.css('.quote').map(&:text).reject(&:empty?).each do |quote|
-  puts "#{quote}" + "\n"*2
+Mechanize.new.get("http://bashorg.org/") do |page|
+  page.parser.css('br').each { |br| br.replace("\n") }
+  page.parser.css('.quote').map(&:text).reject(&:empty?).each do |quote|
+    puts "#{quote}#{"\n"*2}"
+  end
 end
 
