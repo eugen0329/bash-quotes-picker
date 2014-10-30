911c8c85e805deda7eec809a1bae23361fd7370b
eugen0329 <eugen0329@gmail.com>
2014-10-30 03:22:40 +0300

    added initial version with br conversion

--- bqpicker.rb
+++ bqpicker.rb
@@ -3,3 +3,11 @@
 require "pry"
 require "colorize"
 require "mechanize"
+
+page = Mechanize.new.get("http://bashorg.org/")
+
+page.parser.css('br').each { |br| br.replace("\n") }
+page.parser.css('.quote').map(&:text).reject(&:empty?).each do |quote|
+  puts "#{quote}" + "\n"*2
+end
+
