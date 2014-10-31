52955b0962837a8a2bcff9c37f03f744f436846b
eugen0329 <eugen0329@gmail.com>
2014-10-31 17:12:55 +0300

    added class and basic methods to scrape quotes, rating and quote number

--- bqpicker.rb
+++ bqpicker.rb
@@ -4,10 +4,29 @@ require "pry"
 require "colorize"
 require "mechanize"
 
-Mechanize.new.get("http://bashorg.org/") do |page|
-  page.parser.css('br').each { |br| br.replace("\n") }
-  page.parser.css('.quote').map(&:text).reject(&:empty?).each do |quote|
-    puts "#{quote}#{"\n"*2}"
+class Bqpicker
+  URL = "http://bashorg.org/"
+
+  def initialize
+    @agent = Mechanize.new { |agent| agent.user_agent = 'Custom agent' }
+  end
+
+  def scrape
+    @agent.get(URL).parser.css(".q").each do |q|
+      q.at_css(".quote").css("br").each { |br| br.replace("\n") }
+      puts "#{q.at_css("a").content}".blue
+      puts "#{q.at_css(".quote").text}\n"
+      puts "♥ #{colorizeVote(q.at_css("span").text.to_i)}\n\n"
+    end
+  end
+
+  private 
+
+  def colorizeVote(rating)
+    color = :default
+    rating.to_i > 0 ? color = :green : color = :red
+    "#{rating}".colorize(color)
   end
 end
 
+Bqpicker.new.scrape
