d4287bb33073600e43468db253406ceac48eab72
eugen0329 <eugen0329@gmail.com>
2014-11-01 17:44:29 +0300

    commands are moved to the methods, added writing to a xml file

--- bqpicker.rb
+++ bqpicker.rb
@@ -11,18 +11,48 @@ class Bqpicker
     @agent = Mechanize.new { |agent| agent.user_agent = 'Custom agent' }
   end
 
-  def scrape
-    @agent.get(URL).parser.css(".q").each do |q|
-      q.at_css(".quote").css("br").each { |br| br.replace("\n") }
-      puts "#{q.at_css("a").content}".blue
-      puts "#{q.at_css(".quote").text}\n"
-      puts "♥ #{colorizeVote(q.at_css("span").text.to_i)}\n\n"
+  def makeNode(doc, content, attr)
+    node = Nokogiri::XML::Node.new("quote", doc) do |node|
+      node.content = content.gsub("\n", "<br />")
+      attr.each { |attr,val| node[attr] = val }
     end
+    node 
+  end
+
+
+  def scrape
+    document =  Nokogiri::XML::Builder.new(:encoding => 'UTF-8') { |xml| xml.root }.doc
+    @agent.get(URL).parser.css(".q").each do |q|
+      attr, content = getNodeData(q)
+      document.root << makeNode(document, content , attr.dup)
+      dispQuote(attr, content)
+    end
+    File.open("./res/quotes.out.xml", "w") { |file| file << document.to_xml }
   end
 
   private 
 
-  def colorizeVote(rating)
+  def getNodeData(q)
+      attr = {
+        head:    q.at_css("a").content, 
+        rating:  q.at_css("span").content
+      }
+      content = getContent(q)
+      return attr, content
+  end
+
+  def dispQuote(attr, content)
+    puts "#{attr[:head]}".blue
+    puts "#{content}"
+    puts "♥ #{colorizeRating(attr[:rating])}\n\n"
+  end
+
+  def getContent(q)
+    q.at_css(".quote").css("br").each { |br| br.replace("\n") }
+    content = q.at_css(".quote").content
+  end
+
+  def colorizeRating(rating)
     color = :default
     rating.to_i > 0 ? color = :green : color = :red
     "#{rating}".colorize(color)
