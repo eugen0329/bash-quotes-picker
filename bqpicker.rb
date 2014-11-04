9b77d1957f5a83212185a204b0585718ca8f24d4
eugen0329 <eugen0329@gmail.com>
2014-11-04 04:15:51 +0300

    added CsvOut module and -outformat option

--- bqpicker.rb
+++ bqpicker.rb
@@ -12,8 +12,9 @@ def parse_args(argv)
   opts = Slop.new(strict: true, help: true) do 
     banner "Usage [OPTIONS]"
     on "v", "verbose", "Verbose mode"
-    on "f=", "ofname=", "Output file",               default: "#{RES_DIR}/quotes.out.xml"
-    on "n=", "num=", "Number of quotes for picking", default: 5
+    on "o=", "ofname=", "Output file",               default: "#{RES_DIR}/quotes.out"
+    on "n=", "num=", "Number of quotes for picking", default: 5,    as: Integer
+    on "-f=", "--outformat=", "Out file format",     default: :xml, as: Symbol
   end
   begin 
     opts.parse!(argv)
@@ -31,9 +32,16 @@ def display_quotes(quotes)
 end
 
 opts = parse_args(ARGV)
-quotes = BashorgQuotesPicker.new.scrape(opts[:num].to_i)
+quotes = BashorgQuotesPicker.new.scrape(opts[:num])
 
 display_quotes(quotes) if opts.verbose?
 
-xml_out_doc = XmlOut::get_xml_doc(quotes, node_name: "quote")
-XmlOut::write(xml_out_doc, opts[:ofname])
+case opts[:outformat]
+when :xml
+  xml_out_doc = XmlOut::get_xml_doc(quotes, node_name: "quote")
+  XmlOut::write(xml_out_doc, opts[:ofname])
+when :csv
+  csv_out = CsvOut::get_csv_string(quotes)
+  CsvOut::write(csv_out, opts[:ofname])
+end
+
---
+++ lib/csv_out.rb
@@ -0,0 +1,26 @@
+require "fileutils"
+require "csv"
+
+module CsvOut
+  #DEFAULT_OPTS = {}
+
+  def write(csv_string, ofname)
+    ofdir = File.expand_path("../", ofname)
+    FileUtils.mkdir_p(ofdir) unless File.directory?(ofdir)
+
+    File.open(ofname, "w") { |f| f << csv_string }
+  end
+
+  def get_csv_string(data, args = {})
+    #opts = DEFAULT_OPTS
+    #args.each { |k,v| opts[k] = v }
+    csv_out_string = CSV.generate do |csv|
+      csv << data.first.map { |k,_| k.to_s }
+      data.each { |elem| csv << elem.map { |_,v| v } }
+    end
+    
+    csv_out_string
+  end
+
+  module_function :write, :get_csv_string
+end
